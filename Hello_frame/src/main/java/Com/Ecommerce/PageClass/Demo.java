450f1d0ae013a592be60caefd38065598fd2b0b7
TajThao <thaotaj50@gmail.com>
2023-02-28 12:25:53 +0530

    QA Commit

---
+++ Hello_frame/src/main/java/Com/Ecommerce/PageClass/Demo.java
@@ -0,0 +1,12 @@
+package Com.Ecommerce.PageClass;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+	    System.out.println("Hello");
+		
+	}
+
+}
